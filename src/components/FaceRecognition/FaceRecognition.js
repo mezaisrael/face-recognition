bbedcdbcd33ad97e99401e32b7e115b9cb20757e
israel <meza.israel.91@gmail.com>
2018-07-20 15:26:29 -0400

    fixed indentation

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -12,11 +12,11 @@ const FaceRecognition = ({imageUrl, box, showBox}) => {
 				<div
 					className='bounding_box'
 					style={{
-								top: box.topRow,
-								left: box.leftCol,
-								right: box.rightCol,
-								bottom: box.bottomRow,
-								visibility: showBox
+							top: box.topRow,
+							left: box.leftCol,
+							right: box.rightCol,
+							bottom: box.bottomRow,
+							visibility: showBox
 							}}>
 				</div>
 			</div>
